00ec1a7105e6758d1c13877cec74b3ee9b5764ae
dhywty <clemwek@gmail.com>
2018-02-20 07:35:56 +0300

    Initial Commit

---
+++ schatapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  schatapp
+//
+//  Created by Dhwty on 20/02/2018.
+//  Copyright © 2018 Dhwty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
